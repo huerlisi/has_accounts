36fa93c7512a6f04aeb35e92b97ae920531c44eb
Simon Huerlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-08-12 15:37:19 +0200

    New Booking class.

---
+++ lib/accounting/booking.rb
@@ -0,0 +1,8 @@
+module Accounting
+  class Booking < ActiveRecord::Base
+    belongs_to :debit_account, :foreign_key => 'debit_account_id', :class_name => "Account"
+    belongs_to :credit_account, :foreign_key => 'credit_account_id', :class_name => "Account"
+
+    belongs_to :reference, :polymorphic => true
+  end
+end
