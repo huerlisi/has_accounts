f570e6b96353dd5bf3600e85879838b4a5a75fb2
Simon Huerlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-08-14 11:22:24 +0200

    Add (credit_|debit_|)bookings associations to Account model.

--- lib/accounting/account.rb
+++ lib/accounting/account.rb
@@ -1,6 +1,11 @@
 module Accounting
   class Account < ActiveRecord::Base
     belongs_to :holder, :polymorphic => true
+    
+    has_many :credit_bookings, :class_name => "Booking", :foreign_key => "credit_account_id"
+    has_many :debit_bookings, :class_name => "Booking", :foreign_key => "debit_account_id"
+    
+    has_many :bookings, :finder_sql => 'SELECT * FROM bookings WHERE credit_account_id = #{id} OR debit_account_id = #{id} ORDER BY value_date'
   end
 
   module ClassMethods
