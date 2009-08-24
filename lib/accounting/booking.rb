3b142d162546cd2b7590b2677000d1a07a18aa35
Simon Huerlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-08-24 22:46:01 +0200

    Add Accounting::Booking.accounted_amount(account).

--- lib/accounting/booking.rb
+++ lib/accounting/booking.rb
@@ -4,5 +4,15 @@ module Accounting
     belongs_to :credit_account, :foreign_key => 'credit_account_id', :class_name => "Account"
 
     belongs_to :reference, :polymorphic => true
+
+    def accounted_amount(account)
+      if debit_account == account
+        return amount
+      elsif credit_account == account
+        return -(amount)
+      else
+        return 0.0
+      end
+    end
   end
 end
