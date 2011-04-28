f23750b8f40cb2e9435c3589183937338637884c
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-28 22:49:25 +0200

    Implement migration generator.

---
+++ lib/generators/has_accounts/migration_generator.rb
@@ -0,0 +1,26 @@
+require 'rails/generators/base'
+require 'rails/generators/migration'
+  
+module HasAccounts
+  class MigrationGenerator < Rails::Generators::Base
+    include Rails::Generators::Migration
+
+    def self.source_root
+      @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+    end
+
+    # Implement the required interface for Rails::Generators::Migration.
+    # taken from http://github.com/rails/rails/blob/master/activerecord/lib/generators/active_record.rb
+    def self.next_migration_number(dirname)
+      if ActiveRecord::Base.timestamped_migrations
+        Time.now.utc.strftime("%Y%m%d%H%M%S")
+      else
+        "%.3d" % (current_migration_number(dirname) + 1)
+      end
+    end
+
+    def create_migration_file
+      migration_template 'migration.rb', 'db/migrate/setup_has_accounts_engine.rb'
+    end
+  end
+end
