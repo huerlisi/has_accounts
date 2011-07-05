917b73bc5a0299242917368eb1ae2b17f2c73fdc
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-07-05 23:55:09 +0200

    Test validations for Account.code and Account.account_type.

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -5,4 +5,10 @@ describe Account do
   
   it { should accept_values_for(:title, "Test", "Test Account!") }
   it { should_not accept_values_for(:title, "", nil) }
+
+  it { should accept_values_for(:code, "test", "test_account") }
+  it { should_not accept_values_for(:code, "", nil) }
+
+  it { should accept_values_for(:account_type, FactoryGirl.build(:current_assets), FactoryGirl.build(:costs) ) }
+  it { should_not accept_values_for(:account_type, nil) }
 end
