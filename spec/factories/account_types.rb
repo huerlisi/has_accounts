ef1cfdd0896a4476591f5270150880d6692449a1
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-07-05 23:54:44 +0200

    Use nested factories in account_types.

--- spec/factories/account_types.rb
+++ spec/factories/account_types.rb
@@ -2,22 +2,22 @@ FactoryGirl.define do
   factory :account_type do
     title 'Test Account Type'
     name 'test'
-  end
   
-  factory :current_assets, :parent => :account_type do
-    title 'Current Assets'
-    name 'current_assets'
-  end
-  factory :outside_capital, :parent => :account_type do
-    title 'Outside Capital'
-    name 'outside_capital'
-  end
-  factory :costs, :parent => :account_type do
-    title 'Costs'
-    name 'costs'
-  end
-  factory :earnings, :parent => :account_type do
-    title 'Earnings'
-    name 'earnings'
+    factory :current_assets do
+      title 'Current Assets'
+      name 'current_assets'
+    end
+    factory :outside_capital do
+      title 'Outside Capital'
+      name 'outside_capital'
+    end
+    factory :costs do
+      title 'Costs'
+      name 'costs'
+    end
+    factory :earnings do
+      title 'Earnings'
+      name 'earnings'
+    end
   end
 end
