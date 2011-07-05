b5fac9062d44a1cdb0074c6ba3259beb69a3c1c6
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-07-05 23:42:49 +0200

    Test Account.title validations.

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,8 @@
+require 'spec_helper'
+
 describe Account do
   subject { FactoryGirl.build :account }
   
-  its(:title) { should == 'Test Account' }
+  it { should accept_values_for(:title, "Test", "Test Account!") }
+  it { should_not accept_values_for(:title, "", nil) }
 end
