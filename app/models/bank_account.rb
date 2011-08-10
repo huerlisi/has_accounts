541f34247f96c65bba8106ff1099ba4dc0d5a39f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-08-10 11:38:32 +0200

    Drop .to_s overloaded method for BankAccount.

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,13 +1,3 @@
 class BankAccount < Account
   belongs_to :bank
-
-  # Standard methods
-  def to_s(value_range = Date.today, format = :default)
-    case format
-    when :short
-      "#{code}: CHF #{sprintf('%0.2f', saldo(value_range).currency_round)}"
-    else
-      "#{title} (#{code}) #{bank.to_s} #{number}: CHF #{sprintf('%0.2f', saldo(value_range).currency_round)}"
-    end
-  end
 end
