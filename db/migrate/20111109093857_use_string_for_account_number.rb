8d53502c748c600344154e8222388b0a0bdc99d0
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-11-09 10:44:30 +0100

    Fix migration.

--- db/migrate/20111109093857_use_string_for_account_number.rb
+++ db/migrate/20111109093857_use_string_for_account_number.rb
@@ -1,9 +1,9 @@
 class UseStringForAccountNumber < ActiveRecord::Migration
   def up
-    change_column :bank_account, :number, :string
+    change_column :accounts, :number, :string
   end
 
   def down
-    change_column :bank_account, :number, :integer
+    change_column :accounts, :number, :integer
   end
 end
