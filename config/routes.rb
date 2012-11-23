79d96d96f6a83ec7ad859b91bde185b261f74f74
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-11-23 11:27:58 +0100

    Include views and controllers for Account and Booking.

---
+++ config/routes.rb
@@ -0,0 +1,34 @@
+# encoding: UTF-8
+
+# Routes
+Rails.application.routes.draw do
+  # Bookings
+  resources :bookings do
+    collection do
+      post :select
+      get :simple_edit
+    end
+    member do
+      get :select_booking
+      get :copy
+    end
+  end
+
+  resources :accounts do
+    member do
+      get :csv_bookings
+      get :edit_bookings
+      post :update_bookings
+    end
+    resources :bookings
+    resources :attachments
+  end
+
+  resources :bank_accounts do
+    member do
+      get :csv_bookings
+    end
+    resources :bookings
+    resources :attachments
+  end
+end
