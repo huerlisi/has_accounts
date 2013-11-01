e4cc8a6c36ea97a52d404a70b05979220d86c2c5
Simon Hürlimann <simon.huerlimann@cyt.ch>
2013-11-01 14:24:35 +0100

    Refactor and enhance factoriesf for tests.

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -3,17 +3,30 @@ FactoryGirl.define do
     code  '0000'
     title 'Test Account'
     association :account_type
-  end
-  
-  factory :accounts_payable, :parent => :account do
-    code  '2000'
-    title 'Accounts Payable'
-    association :account_type, :factory => :outside_capital
-  end
+    initialize_with { Account.find_or_create_by_code(code)}
 
-  factory :cash_account, :parent => :account do
-    code  '1000'
-    title 'Cash'
-    association :account_type, :factory => :current_assets
+    factory :accounts_payable, :parent => :account do
+      code  '2000'
+      title 'Accounts Payable'
+      association :account_type
+    end
+
+    factory :cash_account, :parent => :account do
+      code  '1000'
+      title 'Cash'
+      association :account_type
+    end
+
+    factory :debit_account, :parent => :account do
+      code  '1100'
+      title 'Account Receivable'
+      association :account_type, :factory => :current_assets
+    end
+
+    factory :earnings_account, :parent => :account do
+      code  '3200'
+      title 'Revenue Account'
+      association :account_type, :factory => :earnings
+    end
   end
 end
--- spec/factories/bookings.rb
+++ spec/factories/bookings.rb
@@ -5,5 +5,17 @@ FactoryGirl.define do
     amount         37.50
     association    :debit_account, :factory => :accounts_payable
     association    :credit_account, :factory => :cash_account
+
+    factory :invoice_booking do
+      title 'Invoice'
+      association :debit_account, :factory => :debit_account
+      association :credit_account, :factory => :earnings_account
+    end
+
+    factory :payment_booking do
+      title 'Payment'
+      association :debit_account, :factory => :cash_account
+      association :credit_account, :factory => :debit_account
+    end
   end
 end
